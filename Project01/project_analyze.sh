85305c6112d22017701206f9d3f08d8abd0441ea
GABAH <gabah@mcmaster.ca>
2019-02-26 23:18:43 +0000

    Final Completion of project.... Hopefully!

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-cd ~/CS1XA3/Project01
-
-if [ -e "merge.log" ]; then
-	rm merge.log
-fi
-
 echo "Welcome to the digital world"
 sleep 1s
 echo "What should I call you?"
@@ -17,18 +11,13 @@ sleep 1s
 echo "So I have a few script options available for you"
 echo "Here they are:"
 sleep 0.3s
-echo "1. Create a TODO Log (Under Construction)"
+echo "1. Create a TODO Log"
 sleep 0.2s
-echo "2. Compile Error Log (Under Construction)"
+echo "2. Merge Log"
 sleep 0.2s
-echo "3. Merge Log"
+echo "3. File Type Count"
 sleep 0.2s
-echo "4. File Type Count (Under Construction)"
-sleep 0.2s
-echo "5. Delete Temporary Files (Under Construction)"
-sleep 0.2s
-echo "6. Find Last Working File (Under Construction)"
-
+echo "4. Directory, Files and Links Count"
 sleep 0.2s
 
 echo "What job would Monsieur/Madame $user like me to do for them?? (Please use the index numbers)"
@@ -37,10 +26,53 @@ sleep 0.2s
 read script
 sleep 0.1s
 
-if [ $script -eq 3 ]; then
+if [ $script -eq 1 ]; then
+	cd ~/CS1XA3
+	if [ -e "todo.log" ];then
+		rm todo.log
+	fi
+	echo 'Your output would be ready in a second'
+	find -name "*" -type f | xargs grep -i "TODO" --exclude=todo.log --exclude=project_analyze.sh --exclude=README.md > todo.log
+	mv ./todo.log ./Project01/todo.log
+fi
+if [ $script -eq 2 ]; then
+	if [ -e "merge.log" ];then
+		rm merge.log
+	fi
 	echo "Very Well Monsieur/Madame $user"
 	echo 'Give me a second.... Your output will be found in a file named merge.log'
 	git log -i --oneline --grep="merge" | cut -d' ' -f1 > merge.log
-else
-	echo "Its Broken"
+fi
+if [ $script -eq 3 ]; then
+	echo "Be right back with your output!"
+	sleep 0.2s
+	cd ~/CS1XA3/
+	html=$(find . -name "*.hs" -type f | wc -l)
+	js=$(find . -name "*.js" -type f | wc -l)
+	css=$(find . -name "*.css" -type f | wc -l)
+	py=$(find . -name "*.py" -type f | wc -l)
+	hs=$(find . -name "*.hs" -type f | wc -l)
+	sh=$(find . -name "*.sh" -type f | wc -l)
+	echo "HTML: $html"
+	echo "JavaScript: $js"
+	echo "CSS: $css"
+	echo "Python: $py"
+	echo "Haskell: $hs"
+	echo "Bash Script: $sh"
+fi
+if [ $script -eq 4 ]; then
+	if [ -e "item_count.log" ]; then
+		rm item_count.log
+	fi
+	echo "Sure thing, What directory would you like me to search? (absolute path is preferrable)"
+	read dir
+	cd ~/
+	tfiles=$(ls -l $dir | wc -l)
+	echo "You have $tfiles files in $dir"
+	tf=$(ls -l $dir | grep -c ^-)
+	td=$(ls -l $dir | grep -c ^d)
+	tl=$(ls -l $dir | grep -c ^l)
+	"Files: $tf" > item_count.log
+	"Directories: $td" > item_count.log
+	"Links: $tl" > item_count.log
 fi
