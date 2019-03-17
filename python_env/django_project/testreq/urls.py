776a48876d466eb75b5121f0dfc12ec6e0a0f548
GABAH <gabah@mcmaster.ca>
2019-03-17 19:14:37 +0000

    redo

--- python_env/django_project/testreq/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-	path('',views.hello,name="testreq-hello"),
-	path('',views.getrequest,name="testreq-getreqeuest"),
-	path('',views.postrequest,name="testreq-getpost"),
-]
